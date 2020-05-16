b18873e7338921de9e69269fb6a98b876dd55040
Robert Konigsberg <konigsberg@gmail.com>
2020-05-16 14:26:17 -0400

    Monte carlo simulation for the Wingspan automa and code to graph the results

---
+++ graph_simulations.py
@@ -0,0 +1,118 @@
+from collections import namedtuple, Counter
+import csv
+from cycler import cycler
+import matplotlib
+import matplotlib.pyplot as plt
+import statistics
+import matplotlib.ticker as plticker
+
+Play = namedtuple(
+  'Play',
+  ['parameters', 'p_ppb', 'p_autombon', 'p_deck', 'p_goal',
+   'eggs', 'birds', 'game_end_bonus', 'round_end_bonus', 'total'])
+
+def read():
+  print("Reading")
+  rows = []
+  with open('scores.csv', newline='') as csvfile:
+      reader = csv.reader(csvfile, delimiter=',')
+      next(reader) # skip header, todo compare header with namedtuple to verify
+                   # fields are mapped.
+      for row in reader:
+        play = Play(
+          parameters = row[0],
+          p_ppb = int(row[1]),
+          p_autombon = row[2] == 'True',
+          p_deck = row[3],
+          p_goal = row[4],
+          eggs = int(row[5]),
+          birds = int(row[6]),
+          game_end_bonus = int(row[7]),
+          round_end_bonus = int(row[8]),
+          total = int(row[9]))
+        rows.append(play)
+  return rows
+
+class PlotLine:
+  def __init__(self, filter, label, axis=0):
+    self.filter = filter
+    self.label = label
+    self.axis = axis
+
+def calculate(rows, filter, field='total'):
+  c = Counter()
+  for row in rows:
+    key = row.total # row[field] getattr(row,field) ?
+    if filter(row) == True:
+      c[key] = c[key] + 1
+  return c
+
+def p_all(): return PlotLine(lambda _ : True, 'all')
+def p_deck(v): return PlotLine(lambda x : x.p_deck == v, f'deck[{v}]')
+def p_ppb(v): return PlotLine(lambda x : x.p_ppb == v, f'ppb[{v}]')
+def p_autombon(v): return PlotLine(lambda x : x.p_autombon == v, f'autumbon[{v}]')
+def p_goal(v): return PlotLine(lambda x : x.p_goal == v, f'goal[{v}]')
+def axis(axis, plotline):
+  plotline.axis = axis
+  return plotline
+
+def go():
+  rows = read()
+
+  def graph(title, basename, *plots):
+    fig, ax = plt.subplots()
+    ax.get_yaxis().set_ticklabels([]) # Hide y axis values
+    ax.xaxis.set_minor_locator(plticker.MultipleLocator(base=2.0))
+
+    ax.grid(which='minor', linestyle=':', linewidth='0.5', color='gray')
+
+    def addPlot(plotline, nth):
+      print("Plot")
+      result = calculate(rows, plotline.filter)
+      mean = statistics.mean(result.elements())
+      stdev = statistics.stdev(result.elements())
+      height = max(result.values())
+      xs = sorted(result.keys())
+      ys = [result[x] for x in xs]
+  
+      # Choose color here so the plot and error bars share it.
+      color = next(ax._get_lines.prop_cycler)['color']
+      ax.plot(
+        xs,
+        ys,
+        color=color,
+        label=plotline.label)
+
+      ax.errorbar(
+        mean,
+        (height / 2) - (nth * 1000),
+        xerr=stdev,
+        snap=True,
+        color=color,
+        marker='^')
+
+    nth = 0
+    for plot in plots:
+      addPlot(plot, nth)
+      nth = nth + 1
+
+    ax.set(xlabel='score', ylabel='count', title=title)
+
+    ax.legend()
+    ax.grid()
+
+    fig.savefig(f'{basename}.png')
+    # plt.show()
+
+  graph('By ppb', 'byppb', p_ppb(3,), p_ppb(4), p_ppb(5))
+  graph('By deck', 'bydeck', p_deck('base'), p_deck('ee'), p_deck('both'))
+  graph('With Autumbon Society', 'byautumbon', p_autombon(False), p_autombon(True))
+  graph('By goal', 'bygoal', p_goal('high'), p_goal('low'))
+  graph('all', 'all',
+     p_all(),
+     axis(1, p_ppb(3)), axis(1, p_ppb(4)), axis(1, p_ppb(5)),
+     axis(2, p_deck('base')), axis(2, p_deck('ee')), axis(2, p_deck('both')),
+     axis(3, p_autombon(False)), axis(3, p_autombon(True)),
+     axis(4, p_goal('high')), axis(4, p_goal('low')))
+
+go()
---
+++ monte-carlo.py
@@ -0,0 +1,244 @@
+import random
+import sys
+import csv
+
+def log(text):
+  if (False):
+    print(text)
+
+class Automa:
+  # Each card has three characters. The first is a digit used for eggs, to indicate
+  # how many eggs to include. 
+  # d = die from the birdfeeder
+  # e = take eggs
+  # c = get card
+  # g = get goal card
+  # p = activate pink powers
+  # + / -, change end of round goals tokens.
+  automa_decks = [
+    [" d+", "1e-", " c+", " g ", " g ", "1e ", "1e+", " dp", " dp", " c-", " g+"],
+           [" c+", "2e-", "2e-", " c ", " g+", " dp", " g ", "2e+", " dp", " g+"],
+                  [" g+", " c-", " dp", " g+", "3e-", "2e+", " c ", " dp", " g+"],
+                         [" dp", "3e+", "3e+", " c-", " c+", "3e-", " g ", " g+"],
+
+  ]
+
+  def highone(self, display):
+    opts = list(filter(lambda x: x in [5,6,7], display))
+    if opts:
+      c = max(opts)
+      return [c]
+    return None
+
+  def lowone(self, display):
+    cs = list(filter(lambda x: x in [3,4], display)) # list().sort()
+    if cs:
+      cs.sort()
+      result = cs[-2:]
+      return result
+    return None
+
+  def __init__(self):
+    self.eggs = 0
+    self.birds = 0
+    self.bonus_birds = []
+    self.round_bonuses = []
+    self.cubes = 0
+    self.deck = []
+
+  def initRound(self, round, use_autombon):
+    if use_autombon:
+      self.automa_deck = self.automa_decks[round][:] # [:] makes a copy
+    else:
+      self.automa_deck = self.automa_decks[round][:-1] # [:] makes a copy
+    log(f'hey{self.automa_deck}')
+    random.shuffle(self.automa_deck)
+    self.cubes = 0
+
+  def playCard(self, game, goal):
+    card = self.automa_deck.pop()
+    items = list(card)
+    eggmod = items[0]
+    action = items[1]
+    rhs = items[2]
+    log(f'Drew {card}')
+    if action == 'e':   
+      self.eggs += int(eggmod)
+
+    if action == 'c':
+      self.birds += 1
+      game.resetDisplay()
+
+    if action == 'g':
+      if goal == 'high':
+        result = self.highone(game.display)
+      elif goal == 'low':
+        result = self.lowone(game.display)
+      else:
+        raise "Yo"
+      if result and len(result) > 0:
+        for x in result:
+          self.bonus_birds.append(x)
+          game.display.remove(x) 
+        game.fillDisplay()
+      else:
+        self.birds += 1
+  
+    if rhs == '+':
+      self.cubes += 1
+    if rhs == '-':
+      self.cubes = max(0, self.cubes - 1)
+
+  def scoreRound(self, round):
+    higher = [4, 5, 6, 7]
+    lower = [1, 2, 3, 4]
+    # +/-: goals, well, they're funny. Making an estimate, if the result is +2 or more, Automa wins.
+    # +1, tie, 0, human wins.
+    if self.cubes >= 2:
+      round_bonus = higher[round]
+    if self.cubes == 1:
+      round_bonus = int((higher[round] + lower[round]) / 2)
+    if self.cubes == 0:
+      round_bonus = lower[round]
+
+    self.round_bonuses.append(round_bonus)
+
+  def score(self, parameters):
+    log("\n\nscore")
+    scores = {
+        'eggs': self.eggs,
+        'birds': self.birds * parameters['ppb'],
+        'game-end-bonus': sum(self.bonus_birds),
+        'round-end-bonus': sum(self.round_bonuses)
+    }
+
+    scores['total'] = sum(scores.values())
+    for i in scores:
+      log (f'{i}: {scores[i]}')
+    return scores
+
+class Game:
+  bird_cards = [
+    { 0:6, 1:12, 2:20, 3:33, 4:33, 5:34, 6:12, 7:8, 8:8, 9:5 }, # base OFF BY ONE
+    { 0:3, 1:7,  2:8,  3:18, 4:24, 5:8,  6:6,  7:4, 8:2, 9:0 }  # ee
+  ]
+
+  def __init__(self, parameters):
+    self.parameters = parameters
+    self.makeDeck()
+    self.display = []
+    self.automa = Automa()
+    self.round = -1
+    self.prepareRound()
+
+  def prepareRound(self):
+    self.round = self.round + 1
+    log(f'\nRound {self.round}\n')
+    self.automa.initRound(self.round, self.parameters['autombon'])
+    self.resetDisplay()
+
+  def resetDisplay(self):
+    self.display = []
+    self.fillDisplay()
+
+  def fillDisplay(self):
+    while len(self.display) < 3:
+      self.display.append(self.deck.pop())
+
+  def makeDeck(self):
+    self.deck = []
+    def add_all(cards):
+      for points in cards:
+        self.deck = self.deck + ([points] * cards[points])
+
+    log(self.parameters)
+    if (self.parameters['deck']) in ('base', 'both'):
+      add_all(self.bird_cards[0])
+    if (self.parameters['deck']) in ('ee', 'both'):
+      add_all(self.bird_cards[1])
+    random.shuffle(self.deck)
+
+  def playTurn(self):
+    # human removes 0, 1 or 2 cards.
+    x = random.random()
+    if (x < .2):
+      self.deck.remove(random.choice(self.deck))
+    if (x < .4):
+      self.deck.remove(random.choice(self.deck))
+    self.fillDisplay()
+
+    self.automa.playCard(self, self.parameters['goal'])
+
+  def completeRound(self):
+    self.automa.scoreRound(self.round)
+
+# How to score:
+#
+# d: taking a die from the birdfeeder does not impact the Automa's score
+# e: add the number of eggs to the end score. +1 per egg.
+# c: add to the number of birds scored, which will be multipled by its
+#    multiplier (3, 4, 5). Replace the birds
+# g: evaluate bonus condition and take cards. Keep a tally of points.
+# p: pink powers don't really touch the player.
+# +/-: goals, well, they're funny. Making an estimate, if the result is +2 or more, Automa wins.
+# +1, tie, 0, human wins.
+#
+# Every other turn, replace 1 or 2 birds simulating player taking birds. (20% 20%)
+# At the start of a round, replace the birds.
+#
+
+def run_simulation(params, writer):
+  print(params)
+
+  fmtdparams = '{c[ppb]}-{c[autombon]}-{c[deck]}-{c[goal]}'.format(c = params)
+
+  pp_params = dict((f'p_{name}', val) for name, val in params.items())
+
+  for x in range(0, 50000):
+    if x % 5000 == 0:
+      print(f' #{x}')
+    g = Game(params)
+    log(g.automa.automa_decks)
+    for i in range(0, 8):
+      g.playTurn()
+    g.completeRound()
+    g.prepareRound()
+    for i in range(0, 7):
+      g.playTurn()
+    g.completeRound()
+    g.prepareRound()
+    for i in range(0, 6):
+      g.playTurn()
+    g.completeRound()
+    g.prepareRound()
+    for i in range(0, 5):
+      g.playTurn()
+    g.completeRound()
+    data = g.automa.score(params)
+    writer.writerow({**data, **{'parameters': fmtdparams}, **pp_params})
+
+def run_all():
+  ppbs = [3,4,5]
+  autombons = [True, False]
+  decks = ['base', 'ee', 'both']
+  goals = ['high', 'low']
+
+  csv_columns = ['parameters', 'p_ppb', 'p_autombon', 'p_deck', 'p_goal', 'eggs', 'birds', 'game-end-bonus', 'round-end-bonus', 'total']
+  try:
+    with open('scores.csv', 'w') as csvfile:
+      writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+      writer.writeheader()
+      for ppb in ppbs:
+        for autombon in autombons:
+          for deck in decks:
+            for goal in goals:
+              params = { 'ppb': ppb, 'autombon': autombon, 'deck': deck, 'goal': goal}
+              run_simulation(params, writer)
+  except IOError:
+    print(f'Error! {e}')
+
+def main():
+  run_all()
+
+if __name__ == "__main__":
+  main()
